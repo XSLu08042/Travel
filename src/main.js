7a5c917f1eea4f8deef07fbfbb29a4f2b3d6177d
XSLu08042 <183783941@qq.com>
2019-07-19 20:51:46 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
-
+fastClick.attach(document.body)
 /* eslint-disable no-new */
+/* 1、//ES6的写法键和值相同可以省略一个，这里相当于router:router
+2、components: { App }, //这是ES6的写法，
+  //components: { 外层Vue实例给这个组件起的名字： 局部组件名 },
+  3、路由就是根据网址的不同，返回不同的内容给用户
+ */
 new Vue({
   el: '#app',
   router,
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      hello world
+    </diV>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
+
