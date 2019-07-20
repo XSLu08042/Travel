9c8a51f74a4db7025805eecbea1eb66b3ade66fb
XSLu08042 <1831783941@qq.com>
2019-07-20 13:45:22 +0800

    change

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 /* eslint-disable no-new */
 /* 1、//ES6的写法键和值相同可以省略一个，这里相当于router:router
 2、components: { App }, //这是ES6的写法，
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,48 @@
+<template>
+<diV class="swiper">
+  <swiper :options="swiperOption">
+    <swiper-slide v-for="item in swiperList" v-bind:key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+</swiper> 
+</diV>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data(){
+        return{
+            swiperOption:{
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [
+                {
+                    id: '0001',
+                    imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/ae8987bff39ff10e82675a3643154e66.jpg_750x200_0f187b2e.jpg'
+                },
+                {
+                    id: '0001',
+                    imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/6e07f11d492a880c2b5fb9889b3cb008.jpg_750x200_e0e88021.jpg'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .swiper >>> .swiper-pagination-bullet-active
+        background: #ffffff
+    .swiper
+        overflow: hideen
+        width: 100%
+        height: 0
+        padding-bottom: 26.67%
+        background: #eee
+        .swiper-img
+            width: 100%
+</style>
+
